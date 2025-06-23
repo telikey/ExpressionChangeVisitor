5fd3d4497deeb96d07443d5f12be1a46dc08b52c
telikey <49363725+telikey@users.noreply.github.com>
2025-06-23 03:27:28 +0300

    Init

---
+++ ExpressionHelper/ExpressionTypeVisitor.cs
@@ -0,0 +1,335 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using System.Xml.Linq;
+
+namespace ExpressionHelper
+{
+    public class ExpressionChangeVisitorParams
+    {
+        public Type FromType { get; set; }
+        public Type ToType { get; set; }
+
+        public Func<object?, object?> ConstantChangeFunction { get; set; }
+        public Func<MemberInfo?, MemberInfo?> PropertyChangeFunction { get; set; }
+    }
+
+    public class ExpressionChangeVisitorVisitReturn
+    {
+        public Expression Expression { get; set; }
+        public List<(Expression From, Expression To)> ChangedParameters { get; set; }
+    }
+
+    public static class ExpressionChangeVisitor
+    {
+        public static Expression? Visit(Expression? node, ExpressionChangeVisitorParams @params)
+        {
+            return InternalVisit(node, @params)?.Expression;
+        }
+
+        public static ExpressionChangeVisitorVisitReturn? InternalVisit(Expression? node, ExpressionChangeVisitorParams @params)
+        {
+            if (node == null)
+            {
+                return null;
+            }
+
+            FillDefaultParams(@params);
+
+            return InternalVisit(
+                new ExpressionChangeVisitorVisitReturn()
+                {
+                    Expression = node,
+                    ChangedParameters = new List<(Expression From, Expression To)>()
+                }, @params);
+        }
+
+        private static ExpressionChangeVisitorVisitReturn InternalVisit(ExpressionChangeVisitorVisitReturn node, ExpressionChangeVisitorParams @params)
+        {
+            FillDefaultParams(@params);
+
+            if (node.Expression == null)
+            {
+                return node;
+            }
+
+            if(node.Expression is MethodCallExpression)
+            {
+                return VisitMethodCall(node, @params);
+            }
+
+            if (node.Expression is LambdaExpression)
+            {
+                return VisitLambda(node, @params);
+            }
+
+            if(node.Expression is ParameterExpression)
+            {
+                return VisitParameter(node, @params);
+            }
+
+            if (node.Expression is MemberExpression)
+            {
+                return VisitMember(node, @params);
+            }
+
+            if(node.Expression is ConstantExpression)
+            {
+                return VisitConstant(node, @params);
+            }
+
+            if (node.Expression is UnaryExpression)
+            {
+                return VisitUnary(node, @params);
+            }
+
+            if (node.Expression is BinaryExpression)
+            {
+                return VisitBinary(node, @params);
+            }
+
+            throw new Exception("ExpressionTypeVisitor: Can't find nodeType:"+node.Expression.NodeType);
+
+        }
+
+        private static ExpressionChangeVisitorVisitReturn VisitMethodCall(ExpressionChangeVisitorVisitReturn nodeArgs, ExpressionChangeVisitorParams @params)
+        {
+            var node = nodeArgs.Expression as MethodCallExpression;
+
+            var oldArgs = node.Arguments;
+            var oldMethod = node.Method;
+            var oldObject = node.Object;
+            var newArgs = oldArgs?.Select(x =>
+            {
+                nodeArgs.Expression = x;
+                return InternalVisit(nodeArgs, @params).Expression;
+            })?.ToArray();
+
+            nodeArgs.Expression = oldObject;
+            var newObject = InternalVisit(nodeArgs, @params).Expression;
+
+            if (oldMethod.IsGenericMethod)
+            {
+                var newMethod = MakeGenericMethod(oldMethod, @params);
+                newArgs = MakeArgsForCall(newArgs, newMethod.GetParameters().Select(x=>x.ParameterType)).ToArray();
+                var newNode = Expression.Call(newObject,newMethod,newArgs);
+                nodeArgs.Expression = newNode;
+                return nodeArgs;
+            }
+            else
+            {
+                nodeArgs.Expression = node.Update(newObject, newArgs);
+                return nodeArgs;
+            }
+        }
+
+        private static IEnumerable<Expression> MakeArgsForCall(IEnumerable<Expression> args,IEnumerable<Type> argNeedTypes)
+        {
+            var needTypes = argNeedTypes.ToArray();
+            foreach (var arg in args.Select((value, i) => (value, i)))
+            {
+                var needType = needTypes[arg.i];
+                yield return MakeArgForCall(arg.value, needType);
+            }
+        }
+
+        private static Expression MakeArgForCall(Expression node, Type argNeedType)
+        {
+            if (node is LambdaExpression lambdaExpression)
+            {
+                if (argNeedType.IsAssignableTo(typeof(Expression)))
+                {
+                    if (argNeedType.IsGenericType)
+                    {
+                        argNeedType = argNeedType.GetGenericArguments()[0];
+                    }
+                }
+                return Expression.Lambda(argNeedType, lambdaExpression.Body, lambdaExpression.TailCall, lambdaExpression.Parameters);
+            }
+
+            if(node is UnaryExpression unaryExpression)
+            {
+                return unaryExpression.Update(MakeArgForCall(unaryExpression.Operand, argNeedType));
+            }
+
+            return node;
+        }
+
+        private static ExpressionChangeVisitorVisitReturn VisitLambda(ExpressionChangeVisitorVisitReturn nodeArgs, ExpressionChangeVisitorParams @params)
+        {
+            var node = nodeArgs.Expression as LambdaExpression;
+
+            var oldParams = node.Parameters;
+            var oldBody = node.Body;
+
+            var newParams = oldParams?.Select(x => {
+                nodeArgs.Expression = x;
+                return InternalVisit(nodeArgs, @params).Expression as ParameterExpression; 
+            })?.ToArray();
+
+            nodeArgs.Expression = oldBody;
+            var newBody = InternalVisit(nodeArgs, @params).Expression;
+
+            nodeArgs.Expression = Expression.Lambda(newBody, node.Name, node.TailCall, newParams);
+            return nodeArgs;
+        }
+
+        private static ExpressionChangeVisitorVisitReturn VisitMember(ExpressionChangeVisitorVisitReturn nodeArgs, ExpressionChangeVisitorParams @params)
+        {
+            var node = nodeArgs.Expression as MemberExpression;
+
+            var oldContaining = node.Expression;
+            var oldMember = node.Member;
+
+            nodeArgs.Expression = oldContaining;
+            var newContaining = InternalVisit(nodeArgs, @params).Expression;
+            var newMember = oldMember;
+            newMember = @params.PropertyChangeFunction(oldMember);
+
+            nodeArgs.Expression = Expression.MakeMemberAccess(newContaining, newMember);
+            return nodeArgs;
+        }
+
+        private static ExpressionChangeVisitorVisitReturn VisitParameter(ExpressionChangeVisitorVisitReturn nodeArgs, ExpressionChangeVisitorParams @params)
+        {
+            var find = nodeArgs.ChangedParameters.FirstOrDefault(x => x.From == nodeArgs.Expression);
+            if (find.To != null)
+            {
+                nodeArgs.Expression = find.To;
+                return nodeArgs;
+            }
+
+            var node = nodeArgs.Expression as ParameterExpression;
+
+            var oldType = node.Type;
+            var newType = MakeType(oldType, @params);
+            if (node.IsByRef)
+            {
+                nodeArgs.Expression = Expression.Parameter(newType, node.Name);
+            }
+            else
+            {
+                nodeArgs.Expression = Expression.Variable(newType, node.Name);
+            }
+
+            nodeArgs.ChangedParameters.Add((From: node, To: nodeArgs.Expression));
+            return nodeArgs;
+        }
+
+        private static ExpressionChangeVisitorVisitReturn VisitConstant(ExpressionChangeVisitorVisitReturn nodeArgs, ExpressionChangeVisitorParams @params)
+        {
+            var find = nodeArgs.ChangedParameters.FirstOrDefault(x => x.From == nodeArgs.Expression);
+            if (find.To != null)
+            {
+                nodeArgs.Expression = find.To;
+                return nodeArgs;
+            }
+
+            var node = nodeArgs.Expression as ConstantExpression;
+
+            var oldValue = node.Value;
+            var newValue = @params.ConstantChangeFunction(node.Value);
+
+            nodeArgs.Expression = Expression.Constant(newValue);
+            nodeArgs.ChangedParameters.Add((From: node, To: nodeArgs.Expression));
+
+            return nodeArgs;
+        }
+
+        private static ExpressionChangeVisitorVisitReturn VisitBinary(ExpressionChangeVisitorVisitReturn nodeArgs, ExpressionChangeVisitorParams @params)
+        {
+            var node = nodeArgs.Expression as BinaryExpression;
+
+            var oldLeft = node.Left;
+            var oldConvertion = node.Conversion;
+            var oldRight = node.Right;
+            var oldMethod = node.Method;
+
+            nodeArgs.Expression = oldLeft;
+            var newLeft = InternalVisit(nodeArgs, @params).Expression;
+
+            nodeArgs.Expression = oldRight;
+            var newRight = InternalVisit(nodeArgs, @params).Expression;
+
+            nodeArgs.Expression = oldConvertion;
+            var newConversion = InternalVisit(nodeArgs, @params).Expression as LambdaExpression;
+            var newMethod = MakeGenericMethod(oldMethod, @params);
+
+            nodeArgs.Expression = Expression.MakeBinary(node.NodeType, newLeft, newRight, node.IsLiftedToNull, newMethod, newConversion);
+            return nodeArgs;
+        }
+
+        private static ExpressionChangeVisitorVisitReturn VisitUnary(ExpressionChangeVisitorVisitReturn nodeArgs, ExpressionChangeVisitorParams @params)
+        {
+            var node = nodeArgs.Expression as UnaryExpression;
+
+            var oldOperand = node.Operand;
+            var oldMethod = node.Method;
+            var oldType = node.Type;
+
+            nodeArgs.Expression = oldOperand;
+            var newOperand = InternalVisit(nodeArgs, @params).Expression;
+            var newMethod = MakeGenericMethod(oldMethod, @params);
+            var newType = node.NodeType == ExpressionType.Quote? oldType:MakeType(oldType, @params);
+
+            nodeArgs.Expression = Expression.MakeUnary(node.NodeType, newOperand, newType, newMethod);
+            return nodeArgs;
+        }
+
+        private static MethodInfo MakeGenericMethod(MethodInfo info, ExpressionChangeVisitorParams @params)
+        {
+            if (info?.IsGenericMethod??false)
+            {
+                var def = info.GetGenericMethodDefinition();
+                var newGenericArgs = info.GetGenericArguments().Select(x =>
+                {
+                    return MakeType(x,@params);
+                }).ToArray();
+                return def.MakeGenericMethod(newGenericArgs);
+            }
+            return info;
+        }
+
+        private static Type MakeType(Type type, ExpressionChangeVisitorParams @params)
+        {
+            if (type?.Name == @params.FromType.Name)
+            {
+                return @params.ToType;
+            }
+
+            if (type?.IsGenericType ?? false)
+            {
+                var oldArgs = type.GetGenericArguments();
+                var newArgs = oldArgs.Select(x => MakeType(x, @params)).ToArray();
+                var def = type.GetGenericTypeDefinition();
+                return def.MakeGenericType(newArgs);
+            }
+
+            return type;
+        }
+
+        private static  void FillDefaultParams(ExpressionChangeVisitorParams @params)
+        {
+            @params.PropertyChangeFunction = (x) =>
+            {
+                if (x?.DeclaringType?.Name == @params.FromType.Name)
+                {
+                    var prop = @params.ToType.GetProperty(x.Name);
+                    if (prop != null)
+                    {
+                        return prop;
+                    }
+
+                    var field = @params.ToType.GetField(x.Name);
+
+                    if (prop != null)
+                    {
+                        return prop;
+                    }
+                }
+                return x;
+            };
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,83 @@
+﻿using ExpressionHelper;
+using System.Linq.Expressions;
+
+namespace Test
+{
+    public class TestClassItemFrom
+    {
+        public long Id { get; set; }
+        public long? GroupId { get; set; }
+    }
+
+    public class TestClassGroupFrom
+    {
+        public long Id { get; set; }
+        public long? ParentId { get; set; }
+    }
+
+    public class TestClassItemTo
+    {
+        public long Id { get; set; }
+        public long? GroupId { get; set; }
+    }
+
+    public class TestClassGroupTo
+    {
+        public long Id { get; set; }
+        public long? ParentId { get; set; }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var queryItems = (new[] { 
+                new TestClassItemTo() { Id=1, GroupId = 1 }, 
+                new TestClassItemTo() { Id=2, GroupId = 1 }, 
+                new TestClassItemTo() { Id = 3,GroupId=1 } 
+            }).AsQueryable();
+
+            var queryGroups = (new[] { 
+                new TestClassGroupTo() { Id = 1} 
+            }).AsQueryable();
+
+            Expression exp = GenerateExpression<IQueryable<TestClassItemFrom>,IQueryable<TestClassGroupFrom>, IQueryable<TestClassGroupFrom>>(
+                (x,y)=>x.SelectMany(i=>y.Where(j=>i.GroupId==j.Id)).DistinctBy(x=>x.Id));
+
+            var visitorParams = new ExpressionChangeVisitorParams()
+            {
+                FromType = typeof(TestClassItemFrom),
+                ToType = typeof(TestClassItemTo),
+            };
+
+            var visitorParams2 = new ExpressionChangeVisitorParams()
+            {
+                FromType = typeof(TestClassGroupFrom),
+                ToType = typeof(TestClassGroupTo),
+            };
+
+            var newExp = ExpressionChangeVisitor.Visit(exp, visitorParams);
+            var newExp2 = ExpressionChangeVisitor.Visit(newExp, visitorParams2);
+
+            var compiledExp = (newExp2 as LambdaExpression).Compile(true);
+            var res = compiledExp.DynamicInvoke(queryItems, queryGroups);
+
+            Console.ReadKey();
+        }
+
+        static Expression GenerateExpression<T1,TOut>(Expression<Func<T1, TOut>> exp)
+        {
+            return exp;
+        }
+
+        static Expression GenerateExpression<T1,T2, TOut>(Expression<Func<T1, T2, TOut>> exp)
+        {
+            return exp;
+        }
+
+        static Expression GenerateExpression<T1, T2, T3, TOut>(Expression<Func<T1, T2, T3, TOut>> exp)
+        {
+            return exp;
+        }
+    }
+}
